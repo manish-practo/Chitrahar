eb6b5800f5f0e341d3d644cb50cb061329207b34
manish-practo <77273431+manish-practo@users.noreply.github.com>
2021-01-20 21:36:50 +0530

    Added dependencies ingnored by git

---
+++ Dependencies/MovieNetwork/MovieNetwork/MovieNetwork.h
@@ -0,0 +1,18 @@
+//
+//  MovieNetwork.h
+//  MovieNetwork
+//
+//  Created by Manish Pandey on 20/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieNetwork.
+FOUNDATION_EXPORT double MovieNetworkVersionNumber;
+
+//! Project version string for MovieNetwork.
+FOUNDATION_EXPORT const unsigned char MovieNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieNetwork/PublicHeader.h>
+
+
---
+++ Dependencies/MovieStorage/MovieStorage/MovieStorage.h
@@ -0,0 +1,18 @@
+//
+//  MovieStorage.h
+//  MovieStorage
+//
+//  Created by Manish Pandey on 20/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieStorage.
+FOUNDATION_EXPORT double MovieStorageVersionNumber;
+
+//! Project version string for MovieStorage.
+FOUNDATION_EXPORT const unsigned char MovieStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieStorage/PublicHeader.h>
+
+
