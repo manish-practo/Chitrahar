3f1fd129139afb9d9f03d0b4f26b7ed4c0d9df4c
manish-practo <77273431+manish-practo@users.noreply.github.com>
2021-01-20 21:45:33 +0530

    Added uikit and shared frameworks

---
+++ Dependencies/MovieHubShared/MovieHubShared/MovieHubShared.h
@@ -0,0 +1,18 @@
+//
+//  MovieHubShared.h
+//  MovieHubShared
+//
+//  Created by Manish Pandey on 20/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieHubShared.
+FOUNDATION_EXPORT double MovieHubSharedVersionNumber;
+
+//! Project version string for MovieHubShared.
+FOUNDATION_EXPORT const unsigned char MovieHubSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieHubShared/PublicHeader.h>
+
+
---
+++ Dependencies/MovieUIKit/MovieUIKit/MovieUIKit.h
@@ -0,0 +1,18 @@
+//
+//  MovieUIKit.h
+//  MovieUIKit
+//
+//  Created by Manish Pandey on 20/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieUIKit.
+FOUNDATION_EXPORT double MovieUIKitVersionNumber;
+
+//! Project version string for MovieUIKit.
+FOUNDATION_EXPORT const unsigned char MovieUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieUIKit/PublicHeader.h>
+
+
