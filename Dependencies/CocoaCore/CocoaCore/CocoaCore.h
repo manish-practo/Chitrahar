5607ac867b79b3779440af1874b8cd7907d43391
Manish Pandey <manish.pandey@practo.com>
2021-05-29 09:07:09 +0530

    Added cocoa core dependencies.

---
+++ Dependencies/CocoaCore/CocoaCore/CocoaCore.h
@@ -0,0 +1,18 @@
+//
+//  CocoaCore.h
+//  CocoaCore
+//
+//  Created by Manish Pandey on 29/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaCore.
+FOUNDATION_EXPORT double CocoaCoreVersionNumber;
+
+//! Project version string for CocoaCore.
+FOUNDATION_EXPORT const unsigned char CocoaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaCore/PublicHeader.h>
+
+
