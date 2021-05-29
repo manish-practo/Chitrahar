285081010881ee74f540bc0f2de135b1e2a1992c
Manish Pandey <manish.pandey@practo.com>
2021-05-29 09:09:36 +0530

    Added storage core dependency.

---
+++ Dependencies/StorageCore/StorageCore/StorageCore.h
@@ -0,0 +1,18 @@
+//
+//  StorageCore.h
+//  StorageCore
+//
+//  Created by Manish Pandey on 29/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageCore.
+FOUNDATION_EXPORT double StorageCoreVersionNumber;
+
+//! Project version string for StorageCore.
+FOUNDATION_EXPORT const unsigned char StorageCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageCore/PublicHeader.h>
+
+
