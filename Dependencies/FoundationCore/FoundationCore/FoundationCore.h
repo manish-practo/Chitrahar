c16f8390bd38d08a9938d24d08a99dab0856d3be
Manish Pandey <manish.pandey@practo.com>
2021-05-30 13:30:22 +0530

    Added foundation core framework

---
+++ Dependencies/FoundationCore/FoundationCore/FoundationCore.h
@@ -0,0 +1,18 @@
+//
+//  FoundationCore.h
+//  FoundationCore
+//
+//  Created by Manish Pandey on 30/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoundationCore.
+FOUNDATION_EXPORT double FoundationCoreVersionNumber;
+
+//! Project version string for FoundationCore.
+FOUNDATION_EXPORT const unsigned char FoundationCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoundationCore/PublicHeader.h>
+
+
